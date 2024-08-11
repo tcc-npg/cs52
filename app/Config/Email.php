33d681cf8463ddc0aa29ed0ad23b522e58a035fb
nicko <p.nickogamba@gmail.com>
2024-08-11 14:54:45 +0800

    add auth library

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'social.npg@gmail.com';
+    public string $fromName   = 'Nicko';
     public string $recipients = '';
 
     /**
