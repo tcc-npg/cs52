1b1aa9ba8f85b2d85b0ffdf62aff0c83873c397b
nicko <p.nickogamba@gmail.com>
2024-09-15 10:46:27 +0800

    menu clean up

--- app/Views/partials/menu-students.php
+++ app/Views/partials/menu-students.php
@@ -4,34 +4,34 @@
         <div>Students</div>
     </a>
     <ul class="menu-sub">
-        <li class="menu-item <?= url_is('students') ? 'active' : ''; ?>">
-            <li class="menu-item <?= url_is('students*') ? 'open' : ''; ?>">
+        <li class="menu-item <?= is_active_menu('students'); ?>">
+            <li class="menu-item <?= is_active_menu('students*'); ?>">
                 <a href="javascript:void(0);" class="menu-link menu-toggle">
                     <i class="menu-icon tf-icons bx bx-list-ul"></i>
                     <div>List</div>
                 </a>
                 <ul class="menu-sub menu-sub-sub">
-                    <li class="menu-item <?= url_is('students/list/all') ? 'active' : ''; ?>">
+                    <li class="menu-item <?= is_active_menu('students/list/all', true); ?>">
                         <a href="<?= url_to('students.list', 'all'); ?>" class="menu-link">
                             <div>All</div>
                         </a>
                     </li>
-                    <li class="menu-item <?= url_is('students/list/1') ? 'active' : ''; ?>">
+                    <li class="menu-item <?= is_active_menu('students/list/1', true); ?>">
                         <a href="<?= url_to('students.list', 1); ?>" class="menu-link">
                             <div>1st Year</div>
                         </a>
                     </li>
-                    <li class="menu-item <?= url_is('students/list/2') ? 'active' : ''; ?>">
+                    <li class="menu-item <?= is_active_menu('students/list/2', true); ?>">
                         <a href="<?= url_to('students.list', 2); ?>" class="menu-link">
                             <div>2nd Year</div>
                         </a>
                     </li>
-                    <li class="menu-item <?= url_is('students/list/3') ? 'active' : ''; ?>">
+                    <li class="menu-item <?= is_active_menu('students/list/3', true); ?>">
                         <a href="<?= url_to('students.list', 3); ?>" class="menu-link">
                             <div>3rd Year</div>
                         </a>
                     </li>
-                    <li class="menu-item <?= url_is('students/list/4') ? 'active' : ''; ?>">
+                    <li class="menu-item <?= is_active_menu('students/list/4', true); ?>">
                         <a href="<?= url_to('students.list', 4); ?>" class="menu-link">
                             <div>4th Year</div>
                         </a>
@@ -40,7 +40,7 @@
             </li>
         </li>
         <?php if (auth()->user()->inGroup('student')) : ?>
-            <li class="menu-item <?= url_is('user/subjects-enrolled') ? 'active' : ''; ?>">
+            <li class="menu-item <?= is_active_menu('user/subjects-enrolled', true); ?>">
                 <a href="<?= url_to('user.subjects-enrolled'); ?>" class="menu-link">
                     <div>My Subjects</div>
                 </a>
