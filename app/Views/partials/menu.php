dc427d460aaf8a2ade5733e783d1aa6de8da6ba0
Warren Tabora <tws1178@dlsud.edu.ph>
2024-09-18 13:27:25 +0800

    monitoring system first push (module and uniform tab NOT FINAL

--- app/Views/partials/menu.php
+++ app/Views/partials/menu.php
@@ -42,7 +42,6 @@
             </ul>
         </li>
 
-<<<<<<< HEAD
         <!-- Monitoring System -->
        <li class="menu-item">
               <a href="javascript:void(0);" class="menu-link menu-toggle">
@@ -80,9 +79,9 @@
             </ul>
         </li>
 
-=======
+
         <!-- Students menu -->
->>>>>>> dbe32bbbe2b7249d2e6f6622b2e921be52852268
+
         <?php if (auth()->user()->inGroup('admin')): ?>
             <?= $this->include('partials/menu-students')  ;?>
         <?php endif; ?>
